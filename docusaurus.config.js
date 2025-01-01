a2fb97c3f22f35b925b250ea40d2c45fc57c9978
LunaStev <youngjae681@gmail.com>
2025-01-01 19:06:07 +0900

    Wave Website

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://wave-lang.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/wave-lang/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
