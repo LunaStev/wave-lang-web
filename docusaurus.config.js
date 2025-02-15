c5934b8197cafd3c2fad9754a60cb50d2947690d
LunaStev <youngjae681@gmail.com>
2025-02-15 14:04:34 +0900

    Remove

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,7 +49,6 @@ const config = {
       중국어(간체) 'zh-CN',
       중국어(번체) 'zh-TW',
       광동어 'yue',
-      아카드어 'akk',
       에스페란토 'eo',
     */
     locales:
@@ -68,7 +67,6 @@ const config = {
           'zh-CN',
           'zh-TW',
           'yue',
-          'akk',
           'eo',
         ],
     localeConfigs: {
