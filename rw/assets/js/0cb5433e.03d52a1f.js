eda823a788767d7c822f16ce1f2b9c354f30ef64
LunaStev <96914208+LunaStev@users.noreply.github.com>
2025-02-27 05:24:42 +0000

    Deploying to gh-pages from @ LunaStev/wave-lang-web@46f796f105a05cea22b260d3c3fffd6d67377360 🚀

---
+++ rw/assets/js/0cb5433e.03d52a1f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwave_lang=self.webpackChunkwave_lang||[]).push([[2636],{6693:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/rw/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
