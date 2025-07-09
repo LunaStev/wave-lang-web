594ab1f0f5b7d7b9d0fe894f983e9afa18e6ffc5
LunaStev <youngjae681@gmail.com>
2025-07-09 12:00:24 +0900

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,8 +28,6 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-
-
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
@@ -244,7 +242,6 @@ const config = {
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
-        additionalLanguages: ['java', 'rust', 'wave'],
       },
     }),
 };
